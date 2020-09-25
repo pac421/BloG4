3be6990e426990545ddc51c753478bb49e058e88
Nadjim <nadjim13-13@hotmail.fr>
2020-09-25 17:36:29 +0200

    Version 1.6

--- BloG4/src/Controller/SecurityController.php
+++ BloG4/src/Controller/SecurityController.php
@@ -2,13 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
+
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder,EntityManagerInterface $entityManager)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+        $this->entityManager = $entityManager;
+        
+    }
+    
     /**
      * @Route("/", name="app_login")
      */
@@ -32,5 +46,41 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+
+
+     /**
+     * @Route("/subscribe", name="subscribe")
+     */
+    public function subscribe(EntityManagerInterface $entityManager,Request $request, AuthenticationUtils $authenticationUtils)
+    {
+
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+        $firstName = $request->request->get('firstname');
+        $lastName = $request->request->get('lastname');
+        $bornDate = $request->request->get('bornDate');
+
+        if (isset($email) & isset($password) & isset($firstName) & isset($lastName) & isset($bornDate) ){
+            $user= new User;
+            $user->setEmail($email);
+            $user->setFirstname($firstName);
+            $user->setLastName($lastName);
+            $user->setPassword($this->passwordEncoder->encodePassword
+            ($user,
+            $password));
+            $user->setBornDate(new \DateTime($bornDate));
+            $user->setRoles($user->getRoles());
+
+            $entityManager=$this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+            
+        }
+         
+        
     }
 }
