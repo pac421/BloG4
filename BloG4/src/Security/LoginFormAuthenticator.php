a058ccf5c56adf332a497a877da08a2932b9733d
Nadjim <nadjim13-13@hotmail.fr>
2020-09-25 14:53:54 +0200

    version 1.5

--- BloG4/src/Security/LoginFormAuthenticator.php
+++ BloG4/src/Security/LoginFormAuthenticator.php
@@ -97,7 +97,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         }
 
              // redirect to some "app_homepage" route - of wherever you want
-     return new RedirectResponse($this->urlGenerator->generate('fils'));
+     return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
