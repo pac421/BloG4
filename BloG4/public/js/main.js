41989c8b0b04083730480f786c49d5be6b9a43c6
pac421 <paul-alexandre.cieslik@hotmail.com>
2020-09-28 16:43:01 +0000

    Intégration page de connexion/inscription et Javascript input date

--- BloG4/public/js/main.js
+++ BloG4/public/js/main.js
@@ -26,3 +26,24 @@ $('#home_order_date').change(function() {
     console.log('evt home_order_date change start');
     console.log(this.checked);
 });
+
+function input_date_focus(el){
+    console.log('fct input_date_focus start');
+    el.type = 'date';
+};
+
+function input_date_blur(el){
+    console.log('fct input_date_blur start');
+    console.log($('#'+el.id));
+    let selector = $('#'+el.id);
+    if(selector[0].value == ''){
+		el.type = 'text';
+		let parent = selector.parent();
+		parent.find('i').removeClass("active");
+		parent.find('input').removeClass("active");
+		parent.find('label').removeClass("active");
+	}
+};
+
+
+
