efa0da2e1fa62d6fe0202c369a494bf6da7474a6
Nadjim <nadjim13-13@hotmail.fr>
2020-09-29 16:42:23 +0200

    Version 1.9

--- BloG4/src/DataFixtures/UserFixtures.php
+++ BloG4/src/DataFixtures/UserFixtures.php
@@ -21,21 +21,73 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+ 
 
-        $manager->flush();
+        $user1 = new User();
 
-        $user = new User();
+                $user1->setEmail("nadjim13-13@homtail.fr");
+                $user1->setFirstname("Admin");
+                $user1->setLastName("Super");
+                $user1->setBornDate(new \DateTime("02/09/1997"));
+                $user1->setRoles(["ROLE_ADMIN"]);
+                $user1->setPassword($this->passwordEncoder->encodePassword(
+                         $user1,
+                        '1234'
+                     ));
 
-                $user->setEmail("superadmin@gamil.fr");
-                $user->setFirstname("Admin");
-                $user->setLastName("Super");
-                $user->setBornDate(new \DateTime("20/12/1996"));
-                $user->setRoles(["ROLE_ADMIN"]);
-                $user->setPassword($this->passwordEncoder->encodePassword(
-                         $user,
+                $manager->persist($user1);
+
+
+                $user2 = new User();
+
+                $user2->setEmail("superadmin@gamil.fr");
+                $user2->setFirstname("Admin");
+                $user2->setLastName("Super");
+                $user2->setBornDate(new \DateTime("20/12/1996"));
+                $user2->setRoles(["ROLE_ADMIN"]);
+                $user2->setPassword($this->passwordEncoder->encodePassword(
+                         $user2,
                         'password'
                      ));
+
+                $manager->persist($user2);
+
+
+
+
+                $user3 = new User();
+
+                $user3->setEmail("superadmin@gamil.fr");
+                $user3->setFirstname("Admin");
+                $user3->setLastName("Super");
+                $user3->setBornDate(new \DateTime("20/12/1996"));
+                $user3->setRoles(["ROLE_ADMIN"]);
+                $user3->setPassword($this->passwordEncoder->encodePassword(
+                         $user3,
+                        'password'
+                     ));
+
+                $manager->persist($user3);
+
+
+
+
+
+                $user4 = new User();
+
+                $user4->setEmail("superadmin@gamil.fr");
+                $user4->setFirstname("Admin");
+                $user4->setLastName("Super");
+                $user4->setBornDate(new \DateTime("20/12/1996"));
+                $user4->setRoles(["ROLE_ADMIN"]);
+                $user4->setPassword($this->passwordEncoder->encodePassword(
+                         $user4,
+                        'password'
+                     ));
+
+                $manager->persist($user4);
+                $manager->flush();
+
     }
+    
 }
